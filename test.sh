5c3eae607a585779bb325dc88d3346901fd04fcd
Liu Songxiang <sxliu@se.cuhk.edu.hk>
2021-06-12 14:11:37 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,8 +7,6 @@ echo ${config}
 
 # =============== One-shot VC ================
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-  #config=./conf/seq2seq_mol_ppg2mel_vctk_libri_oneshotvc_r4_normMel.yaml
-  #config=./conf/bilstm_ppg2mel_vctk_libri_oneshotvc.yaml
   exp_name="$(basename "${config}" .yaml)"
   echo Experiment name: "${exp_name}"
   model_file="$(ls -dt "ckpt/${exp_name}_seed0"/*.pth | head -1 || true)"
