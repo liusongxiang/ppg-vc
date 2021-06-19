f6bea150f3a95d59f27e311f5bde87b00fc8aeb2
Liu Songxiang <sxliu@se.cuhk.edu.hk>
2021-06-19 11:19:44 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,14 +5,16 @@ stage=1
 stop_stage=1
 config=$1
 model_file=$2
+src_wav_dir=$3
+ref_wav_path=$4
 echo ${config}
 
 # =============== One-shot VC ================
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   exp_name="$(basename "${config}" .yaml)"
   echo Experiment name: "${exp_name}"
-  src_wav_dir="/home/shaunxliu/data/cmu_arctic/cmu_us_rms_arctic/wav"
-  ref_wav_path="/home/shaunxliu/data/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0001.wav"
+#   src_wav_dir="/home/shaunxliu/data/cmu_arctic/cmu_us_rms_arctic/wav"
+#   ref_wav_path="/home/shaunxliu/data/cmu_arctic/cmu_us_slt_arctic/wav/arctic_a0001.wav"
   output_dir="vc_gen_wavs/$(basename "${config}" .yaml)"
 
   python convert_from_wav.py \
